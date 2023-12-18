7edeb2a5120c4b5ee76daebc39ec1522f588e558
Ase Deliri <deliri.ase@gmail.com>
2023-12-18 04:27:31 -0500

    fixed some errors for lualine

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -1,6 +1,7 @@
+-- Path Setup for LazyVim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-    -- bootstrap lazy.nvim
+    -- Bootstrap lazy.nvim
     -- stylua: ignore
     vim.fn.system(
         {"git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath}
@@ -8,46 +9,43 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 
-require("lazy").setup(
-    {
-        spec = {
-            -- add LazyVim and import its plugins
-            {"LazyVim/LazyVim", import = "lazyvim.plugins"},
-            -- import any extras modules here
-            {import = "lazyvim.plugins.extras.lang.typescript"},
-            {import = "lazyvim.plugins.extras.lang.json"},
-            {import = "lazyvim.plugins.extras.ui.mini-animate"},
-            {import = "lazyvim.plugins.extras.lang.go"},
-            {import = "lazyvim.plugins.extras.lang.tailwind"},
-            { import = "lazyvim.plugins.extras.ui.alpha" },
-            -- import/override with your plugins
-            {import = "plugins"}
-        },
-        defaults = {
-            -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
-            -- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
-            lazy = false,
-            -- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
-            -- have outdated releases, which may break your Neovim install.
-            version = false -- always use the latest git commit
-            -- version = "*", -- try installing the latest stable version for plugins that support semver
-        },
-        install = {colorscheme = {"tokyonight", "habamax", "catppuccin"}},
-        checker = {enabled = true}, -- automatically check for plugin updates
-        performance = {
-            rtp = {
-                -- disable some rtp plugins
-                disabled_plugins = {
-                    "gzip",
-                    -- "matchit",
-                    -- "matchparen",
-                    -- "netrwPlugin",
-                    "tarPlugin",
-                    "tohtml",
-                    "tutor",
-                    "zipPlugin"
-                }
+-- LazyVim Configuration
+require("lazy").setup({
+    spec = {
+        -- LazyVim and its plugins
+        {"LazyVim/LazyVim", import = "lazyvim.plugins"},
+        
+        -- Extra Modules
+        {import = "lazyvim.plugins.extras.lang.typescript"},
+        {import = "lazyvim.plugins.extras.lang.json"},
+        {import = "lazyvim.plugins.extras.ui.mini-animate"},
+        {import = "lazyvim.plugins.extras.lang.go"},
+        {import = "lazyvim.plugins.extras.lang.tailwind"},
+        {import = "lazyvim.plugins.extras.ui.alpha"},
+        
+        -- Custom Plugin Imports
+        {import = "plugins"}
+    },
+    defaults = {
+        lazy = false, -- Default lazy-loading behavior
+        version = false -- Use latest git commit for plugins
+    },
+    install = {
+        colorscheme = {"tokyonight", "habamax", "catppuccin"} -- Color schemes
+    },
+    checker = {
+        enabled = true -- Automatic plugin updates
+    },
+    performance = {
+        rtp = {
+            -- Disabled runtime plugins
+            disabled_plugins = {
+                "gzip",
+                "tarPlugin",
+                "tohtml",
+                "tutor",
+                "zipPlugin"
             }
         }
     }
-)
+})
--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -3,8 +3,28 @@ return {
   config = function()
     require("lualine").setup({
       options = {
-        theme = 'catppuccin'
-      }
+       
+        section_separators = {'', ''},
+        component_separators = {'', ''}
+      },
+      sections = {
+        lualine_a = {'mode'},
+        lualine_b = {'branch', 'diff', {'diagnostics', sources={'nvim_diagnostic'}}},
+        lualine_c = {'filename'},
+        lualine_x = {'encoding', 'fileformat', 'filetype'},
+        lualine_y = {'progress'},
+        lualine_z = {'location'}
+      },
+      inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = {'filename'},
+        lualine_x = {'location'},
+        lualine_y = {},
+        lualine_z = {}
+      },
+      tabline = {},
+      extensions = {'fugitive'}
     })
   end
 }
